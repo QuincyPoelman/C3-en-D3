334074dc8d916c43cac969a88c5e5953cf0567c9
Jasper Wesseling <131478@student.drenthecollege.nl>
2022-02-02 14:49:02 +0100

    opmerkings pagina begonnen

---
+++ js/function.js
@@ -0,0 +1,13 @@
+
+    function reactie_weergeven(naam, opmerking) {
+
+
+        document.getElementById("reactie_pagina").innerHTML += "<div class=\"row\" style=\"display: flex; justify-content: center;\"> <div class=\"card\" style=\"width: 40rem;\" ><div class=\"card-body\"><h5 class=\"card-title\">"+naam+"</h5><p class=\"card-text\">"+opmerking+"</p></div></div> </div></br>";
+
+    
+    
+    }
+
+
+
+    // 
